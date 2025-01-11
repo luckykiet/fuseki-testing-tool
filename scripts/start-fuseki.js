50ae576873b0873b13105d92ee4e556abc145b18
Tuan Kiet Nguyen <ngntuankiet@gmail.com>
2025-01-11 17:40:49 +0100

    add chmod

--- scripts/start-fuseki.js
+++ scripts/start-fuseki.js
@@ -1,10 +1,22 @@
 const { exec } = require('child_process');
 const os = require('os');
+const fs = require('fs');
 const path = require('path');
 
 const fusekiDir = path.resolve(__dirname, '../fuseki');
+const fusekiShellScript = path.join(fusekiDir, 'fuseki-server.sh');
+
 const isWindows = os.platform() === 'win32';
 const command = isWindows ? 'fuseki-server.bat' : './fuseki-server.sh';
+
+if (!isWindows) {
+    try {
+        fs.chmodSync(fusekiShellScript, '755');
+    } catch (error) {
+        console.error(`Failed to set execute permissions for ${fusekiShellScript}:`, error);
+        process.exit(1);
+    }
+}
 
 const fusekiProcess = exec(command, { cwd: fusekiDir });
 
